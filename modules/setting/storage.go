a14e90b30714b0dca56fc8015cc09788bafc89b0
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-11-27 17:33:43 -0500

    spelling: unknown
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- modules/setting/storage.go
+++ modules/setting/storage.go
@@ -134,11 +134,11 @@ func getStorageSectionByType(rootCfg ConfigProvider, typ string) (ConfigSection,
 	targetType := targetSec.Key("STORAGE_TYPE").String()
 	if targetType == "" {
 		if !IsValidStorageType(StorageType(typ)) {
-			return nil, 0, fmt.Errorf("unknow storage type %q", typ)
+			return nil, 0, fmt.Errorf("unknown storage type %q", typ)
 		}
 		targetSec.Key("STORAGE_TYPE").SetValue(typ)
 	} else if !IsValidStorageType(StorageType(targetType)) {
-		return nil, 0, fmt.Errorf("unknow storage type %q for section storage.%v", targetType, typ)
+		return nil, 0, fmt.Errorf("unknown storage type %q for section storage.%v", targetType, typ)
 	}
 
 	return targetSec, targetSecIsTyp, nil
